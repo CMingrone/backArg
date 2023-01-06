d7dbeed6f09bd6e66b610b81b1e53c67aafdcdb8
CMingrone <carlos_mingrone@hotmail.com>
2023-01-05 23:13:06 -0300

    se agrega archivo dockerfile

--- src/main/java/com/myApp/sprintBoot/Controlador/ControllerAcercaDe.java
+++ src/main/java/com/myApp/sprintBoot/Controlador/ControllerAcercaDe.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins={"https://carlosmingrone-3309d.web.app/"
+@CrossOrigin(origins={"http://localhost:4200/"
                       })
 public class ControllerAcercaDe {
     @Autowired

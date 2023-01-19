a9fcf6d5583993ee6788ecefc8067aebac2db978
CMingrone <carlos_mingrone@hotmail.com>
2023-01-19 15:12:07 -0300

    cambio de cors

--- src/main/java/com/myApp/sprintBoot/Controlador/ControllerEducacion.java
+++ src/main/java/com/myApp/sprintBoot/Controlador/ControllerEducacion.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins={"http://localhost:4200/"})
+@CrossOrigin(origins={"https://carlosmingrone-3309d.web.app/"})
 public class ControllerEducacion {
     @Autowired
     EducacionService educacionService;
